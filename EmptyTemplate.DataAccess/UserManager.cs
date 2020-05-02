e289ef1f902914ef0587b0487c9f897eecfc2f1d
Denys Olleik <dolleik@gmail.com>
2020-05-02 12:58:50 -0400

    Adding data access and busines projects.

---
+++ EmptyTemplate.DataAccess/UserManager.cs
@@ -0,0 +1,17 @@
+﻿using EmptyTemplate.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyTemplate.DataAccess
+{
+    public class UserManager
+    {
+        public User GetUser(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
